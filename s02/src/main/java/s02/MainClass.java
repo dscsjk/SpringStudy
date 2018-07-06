3ff38ace6ba8825b1e79c1c912d73e4c302a2089
dscsjk <dscsjk@naver.com>
2018-07-06 12:40:42 +0900

    First Commit

---
+++ s02/src/main/java/s02/MainClass.java
@@ -0,0 +1,18 @@
+package s02;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		calculator.setFirstNum(15);
+		calculator.setSecondNum(7);
+		
+		calculator.addition();
+		calculator.subtraction();;
+		calculator.multiplication();;
+		calculator.division();
+ 
+	}
+
+}
